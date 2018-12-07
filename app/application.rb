0fc0320e6492d0ee843ed667f7bf6775fda6764a
Sharmaine Requerme <srqrme@gmail.com>
2018-12-07 22:23:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,48 +1,17 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
-  @@cart = []
+  @@songs = [Song.new("Sorry", "Justin Bieber"),
+            Song.new("Hello", "Adele")]
+  #Create a new class array called @@cart to hold any items in your cart
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    elsif req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty"
-      else
-        @@cart.each do |item|
-          resp.write "#{item}\n"
-        end
-      end
-    elsif req.path.match(/add/)
-
-      add_item = req.params["item"]
-      if @@items.include? add_item
-        @@cart << add_item
-        resp.write "added #{add_item}\n"
-      else
-        resp.write "We don't have that item"
-      end
-    else
-      resp.write "Path Not Found"
+    @@songs.each do |song|
+      resp.write "#{song.title}\n"
     end
 
     resp.finish
-  end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
   end
 end
